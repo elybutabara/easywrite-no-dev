8ec85d45f46992b1c81528383d749d0cb681fd0d
John Paul Mayol <rancor.developer@gmail.com>
2020-03-02 23:04:22 +0800

    make the build script as a full production build with installer

--- build/build.js
+++ build/build.js
@@ -10,9 +10,58 @@ const chalk = require('chalk')
 const webpack = require('webpack')
 const config = require('../config')
 const webpackConfig = require('./webpack.prod.conf')
-
+const builder = require('electron-builder')
+const Platform = builder.Platform
 const spinner = ora('building for production...')
 spinner.start()
+
+let buildOptions = {
+  "appId": "com.forfatterskolen.easywrite",
+  "productName": "Easy Write App",
+  "directories": {
+    "output": "output"
+  },
+  "extraResources": [
+    {
+      "from": "./resources",
+      "to": "./",
+      "filter": [
+        "**/*"
+      ]
+    }
+  ],
+  "files": [
+    "**/*",
+    "!**src/*",
+    "!**config/*",
+    "!**icons/*",
+    "!**test/*",
+    "!**static/*",
+    "!*.db",
+    "!**db/*"
+  ],
+  "mac": {
+    "category": "public.app-category.productivity",
+    "target": "dmg",
+    "icon": "build/icons/mac/icon.icns"
+  },
+  "win": {
+    "target": [
+      {
+        "target": "nsis",
+        "arch": [
+          "x64",
+          "ia32"
+        ]
+      }
+    ],
+    "icon": "build/icons/win/icon.ico"
+  },
+  "nsis": {
+    "allowToChangeInstallationDirectory": true,
+    "oneClick": false
+  }
+}
 
 rm(path.join(config.build.assetsRoot, config.build.assetsSubDirectory), err => {
   if (err) throw err
@@ -32,10 +81,21 @@ rm(path.join(config.build.assetsRoot, config.build.assetsSubDirectory), err => {
       process.exit(1)
     }
 
-    console.log(chalk.cyan('  Build complete.\n'))
+    console.log(chalk.cyan('  WebPack complete.\n'))
     console.log(chalk.yellow(
       '  Tip: built files are meant to be served over an HTTP server.\n' +
       '  Opening index.html over file:// won\'t work.\n'
     ))
   })
+
+
+  let target = (process.platform === 'darwin') ? Platform.MAC.createTarget() : Platform.WINDOWS.createTarget();
+
+  builder.build({
+    targets : target,
+    config: buildOptions
+  }).then(() => {
+    console.log(chalk.green('\n\nBuild complete.\n'))
+    process.exit(0);
+  });
 })
