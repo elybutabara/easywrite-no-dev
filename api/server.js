df15d9d340fa7a659d380ae20a65b561bf53462b
John Paul Mayol <rancor.developer@gmail.com>
2020-04-21 23:04:40 +0800

    Fix Running two exe will throw error

--- api/server.js
+++ api/server.js
@@ -4,9 +4,10 @@
 * References about this module:
 * https://expressjs.com/en/starter/basic-routing.html
 * */
-let express = require('express')
-let app = express()
-
+let initexpress = require('express')
+let log = require('electron-log')
+let { app } = require('electron')
+let express = initexpress()
 const fs = require('fs-extra')
 
 const path = require('path')
@@ -14,9 +15,9 @@ const path = require('path')
 // reference: http://expressjs.com/en/5x/api.html#req.body
 const bodyParser = require('body-parser')
 const multer = require('multer') // v1.0.5
-app.use(bodyParser.json({ limit: '10mb' })) // for parsing application/json
-app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
-app.use(function (req, res, next) {
+express.use(bodyParser.json({ limit: '10mb' })) // for parsing application/json
+express.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+express.use(function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*') // update to match the domain you will make the request from
   res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE')
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
@@ -48,26 +49,26 @@ const upload = multer({
   // you might also want to set some limits: https://github.com/expressjs/multer#limits
 })
 
-app.use('/authors', require('./routes/authors'))
-app.use('/users', require('./routes/users'))
-app.use('/book-genres', require('./routes/book-genres'))
-app.use('/relations', require('./routes/relations'))
-app.use('/books', require('./routes/books'))
-app.use('/items', require('./routes/items'))
-app.use('/locations', require('./routes/locations'))
-app.use('/book-genre-collections', require('./routes/book-genre-collections'))
-app.use('/chapters', require('./routes/chapters'))
-app.use('/chapter-versions', require('./routes/chapter-versions'))
-app.use('/characters', require('./routes/characters'))
-app.use('/relation-details', require('./routes/relation-details'))
-app.use('/scenes', require('./routes/scenes'))
-app.use('/scene-versions', require('./routes/scene-versions'))
-app.use('/scene-items', require('./routes/scene-items'))
-app.use('/scene-locations', require('./routes/scene-locations'))
-app.use('/scene-characters', require('./routes/scene-characters'))
-app.use('/author-personal-progress', require('./routes/author-personal-progress'))
+express.use('/authors', require('./routes/authors'))
+express.use('/users', require('./routes/users'))
+express.use('/book-genres', require('./routes/book-genres'))
+express.use('/relations', require('./routes/relations'))
+express.use('/books', require('./routes/books'))
+express.use('/items', require('./routes/items'))
+express.use('/locations', require('./routes/locations'))
+express.use('/book-genre-collections', require('./routes/book-genre-collections'))
+express.use('/chapters', require('./routes/chapters'))
+express.use('/chapter-versions', require('./routes/chapter-versions'))
+express.use('/characters', require('./routes/characters'))
+express.use('/relation-details', require('./routes/relation-details'))
+express.use('/scenes', require('./routes/scenes'))
+express.use('/scene-versions', require('./routes/scene-versions'))
+express.use('/scene-items', require('./routes/scene-items'))
+express.use('/scene-locations', require('./routes/scene-locations'))
+express.use('/scene-characters', require('./routes/scene-characters'))
+express.use('/author-personal-progress', require('./routes/author-personal-progress'))
 
-app.post(
+express.post(
   '/upload/:imgOf/image',
   upload.single('single-picture-file' /* name attribute of <file> element in your form */),
   (req, res) => {
@@ -100,7 +101,21 @@ app.post(
     }
   }
 )
+express.portNumber = 3000;
+function listen(port) {
+  express.portNumber = port;
+  express.listen(port, () => {
+    console.log("server is running on port :" + express.portNumber);
+  }).on('error', function (err) {
+    if(err.errno === 'EADDRINUSE') {
+      log.warn(`----- Port ${port} is busy, trying with port ${port + 1} -----`)
+      app.exit(0)
+      // listen(port + 1)
+    } else {
+      log.error(`Error running in port: ${port}`)
+      log.error(err)
+    }
+  });
+}
 
-app.listen(3000, function () {
-  console.log('Server is running on port 3000')
-})
+listen(express.portNumber);
