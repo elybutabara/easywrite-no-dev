fe6d5f479b0e7a1c29c596c928a65a0b9feb53dd
John Paul Mayol <rancor.developer@gmail.com>
2020-04-22 22:38:28 +0800

    fix paste in mac not working

--- menu.js
+++ menu.js
@@ -12,7 +12,18 @@ const mainMenuTemplate = [
         role: 'close'
       }
     ]
-  }
+  },
+  {
+    label: "Edit",
+    submenu: [
+      { label: "Undo", accelerator: "CmdOrCtrl+Z", selector: "undo:" },
+      { label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", selector: "redo:" },
+      { type: "separator" },
+      { label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:" },
+      { label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
+      { label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
+      { label: "Select All", accelerator: "CmdOrCtrl+A", selector: "selectAll:" }
+    ]}
 ];
 
 // If mac, add empty object on menu
@@ -38,4 +49,5 @@ if(process.env.NODE_ENV!=='production') {
 
 
 /*remove taskbar top menu*/
-module.exports = (process.env.NODE_ENV == 'production') ? Menu.buildFromTemplate([]) : Menu.buildFromTemplate(mainMenuTemplate);
+//module.exports = (process.env.NODE_ENV == 'production') ? Menu.buildFromTemplate([]) : Menu.buildFromTemplate(mainMenuTemplate);
+module.exports = Menu.buildFromTemplate(mainMenuTemplate);
