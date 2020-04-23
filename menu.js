b4d6bf8a77b521fd1aaa75af35e0437ac83e23d9
Ismael Gil Sanchez <ismaelgilz@gmail.com>
2020-04-24 02:58:39 +0800

    Auto sync to true then swap edit and tranlation in menu items

--- menu.js
+++ menu.js
@@ -13,6 +13,18 @@ const mainMenuTemplate = [
     ]
   },
   {
+    label: "Edit",
+    submenu: [
+      { label: "Undo", accelerator: "CmdOrCtrl+Z", selector: "undo:" },
+      { label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", selector: "redo:" },
+      { type: "separator" },
+      { label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:" },
+      { label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
+      { label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
+      { label: "Select All", accelerator: "CmdOrCtrl+A", selector: "selectAll:" }
+    ]
+  },
+  {
     label:'Translations',
     submenu:[
       {label: "English",
@@ -79,18 +91,6 @@ const mainMenuTemplate = [
           mainWindow.webContents.send('SET_TRANSLATION_DOM','sv')
         }
       }
-    ]
-  },
-  {
-    label: "Edit",
-    submenu: [
-      { label: "Undo", accelerator: "CmdOrCtrl+Z", selector: "undo:" },
-      { label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", selector: "redo:" },
-      { type: "separator" },
-      { label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:" },
-      { label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
-      { label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
-      { label: "Select All", accelerator: "CmdOrCtrl+A", selector: "selectAll:" }
     ]
   }
 ];
