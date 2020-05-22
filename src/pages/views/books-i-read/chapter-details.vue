4cedab91fb816862f84c9eb17d8d87a076f35f1f
Kenji <codes.kenjimagto@gmail.com>
2020-05-22 18:00:13 +0800

    Books I Read, Chapter Comments

--- src/pages/views/books-i-read/chapter-details.vue
+++ src/pages/views/books-i-read/chapter-details.vue
@@ -22,7 +22,7 @@
             <div v-bind:class="{ 'active' : tab.active == 'scenes' }" @click="changeTab('scenes')" class="es-chapter-details-tab-item">{{$tc('SCENE', 2).toUpperCase()}}</div>
         </div>
         <div v-if="tab.active === 'content'"  class="es-chapter-details-tab-content">
-            <div v-html="getChapterContent" class="description"></div>
+            <div v-html="getChapterContent" class="description" v-commentbased="commentbased_params"></div>
         </div>
         <div v-if="tab.active === 'scenes'"  class="es-chapter-details-tab-content scene-listing">
             <books-i-read-chapter-scenes :properties="{ book: book, chapter: chapter }"></books-i-read-chapter-scenes>
@@ -31,18 +31,220 @@
 </div>
 </template>
 
-<script>
+<<script>
 import TinyMCE from '../../../components/TinyMCE'
-import BooksIReadChapterScenes from '@/pages/views/books-i-read/chapter-scenes'
-
+import ChapterScenes from '@/pages/views/chapters/chapter-scenes'
+import ChapterVersions from '@/pages/views/chapters/chapter-versions'
+import ChapterCompareVersions from '@/pages/views/chapters/chapter-compare-versions'
 import moment from 'moment'
+import Vue from 'vue'
 
 const {ipcRenderer} = window.require('electron')
 
+Vue.directive('commentbased', {
+  unbind: function (el, binding) {
+    console.log('binding: ', binding)
+    if (binding.vm && binding.vm.$destroy) {
+      binding.vm.$destroy()
+    }
+    if (document.getElementById(binding.commentbased_sidebar_id)) {
+      document.getElementById(binding.commentbased_sidebar_id).remove()
+    }
+  },
+  bind: function (el, binding) {
+    console.log('binding: ', binding)
+    binding.commentbased_sidebar_id = ('commentbased-sidebar-' + Math.random()).replace('.', '')
+    document.body.insertAdjacentHTML('beforeend', `
+    <div v-if="selected_comments_id !== null" id="` + binding.commentbased_sidebar_id + `" class="commentbased-comments" style="position: fixed; top: 0; right: 0; width: 300px; height: 100%; background: #fff; border-left: 1px solid #f1f1f1; padding: 5px 15px; margin: 0;">
+      <br/>
+      <br/>
+      <div class="commentbased-comments-header" style="text-align: right;">
+        <a href="#" v-on:click.prevent="selected_comments_id=null"><i class="fa fa-times"></i></a>
+      </div>
+      <div style="height: calc(100% - 175px); background: #fff; overflow-y: scroll;">
+        <div v-for="(c, k) in comments[selected_comments_id]" v-bind:style="{'border': editingComment==k?'1px solid orange':'1px solid #f1f1f1'}" class="commentbased-comment" style="border: 1px solid #f1f1f1; padding: 15px; margin-bottom: 15px;">
+          <div style="line-height: 0.8; position: relative;">
+            <div class="c-pop-menu-btn" style="position: absolute; top: 0; right: 5px; border: 0 solid #c0c0c0;" v-on:click.prevent="showCommentActions=showCommentActions==k?null:k">
+                <div style="text-align: right; padding: 0; font-weight: bold; cursor: pointer;">
+                    <i class="fa fa-ellipsis-h"></i>
+                    <div class="c-pop-menu" v-show="showCommentActions==k" style="display: block; background: #fff; border: 1px solid #f1f1f1; padding: 3px; border-radius: 3px; margin-top: -1px; line-height: 1.2; font-size: 12px;">
+                        <div v-on:click="editComment(k, $event)" data-c-pop-menu-action="edit" style="text-align: center; color: #c0c0c0; font-weight: normal; cursor: pointer;">Edit</div>
+                        <div v-on:click="deleteComment(k, $event)" style="text-align: center; color: #c0c0c0; font-weight: normal; cursor: pointer;">Delete</div>
+                    </div>
+                </div>
+            </div>
+            <span style="font-size: 11px; font-weight: bold;"> {{ c.user_name }}</span>
+            <br/>
+            <span style="font-size: 11px;"> {{ displayTime(c.created_at) }}</span>
+          </div>
+          <div style="margin-top: 8px; color: #c0c0c0;">
+            {{ c.message }}
+          </div>
+        </div>
+      </div>
+      <div class="commentbased-comments-form" style="position: absolute; bottom: 0; left: 0; width: 100%; padding: 15px;">
+        <textarea v-model="comment_message_new" class="commentbased-comment-new" style="width: 100%; border: 1px solid #f1f1f1;"></textarea>
+        <div style="text-align: right;">
+          <button v-on:click="pushComment($event)">Submit</button>
+        </div>
+      </div>
+    </div>`)
+
+    binding.vm = new Vue({
+      el: '#' + binding.commentbased_sidebar_id,
+      data: function () {
+        var data = {
+          selected_comments_id: null,
+          comments: {},
+          comment_message_new: '',
+          author: {},
+          showCommentActions: null,
+          editingComment: null
+        }
+        return data
+      },
+      computed: {
+        selected_comments: function () {
+          return this.comments[this.selected_comments_id]
+        }
+      },
+      watch: {
+        selected_comments_id: function () {
+          var scope = this
+          this.comment_message_new = ''
+          this.editingComment = null
+          setTimeout(function () {
+            scope.focusMessage()
+          }, 10)
+        }
+      },
+      methods: {
+        displayTime: function (t) {
+          var txt = moment(t).fromNow()
+          txt = txt.replace('a few seconds ago', 'just now')
+          return txt
+        },
+        focusMessage: function () {
+          var elm = document.querySelector('.commentbased-comment-new')
+          if (elm) {
+            elm.focus()
+          }
+        },
+        editComment: function (k, e) {
+          e.preventDefault()
+          this.editingComment = k
+          this.comment_message_new = this.selected_comments[k].message
+          Vue.nextTick(this.focusMessage)
+        },
+        deleteComment: function (k, e) {
+          e.preventDefault()
+          delete this.comments[this.selected_comments_id][k]
+          binding.value.onAddComment()
+        },
+        setAuthor: function (author) {
+          this.author = author
+        },
+        setCommentsJSON: function (commentsJson) {
+          var c = JSON.parse(commentsJson || '{}')
+          this.comments = c
+        },
+        getCommentsJSON: function () {
+          return JSON.stringify(this.comments)
+        },
+        getContent: function () {
+          return el.innerHTML
+        },
+        pushComment: function (e) {
+          e.preventDefault()
+          var scope = this
+
+          var message = scope.comment_message_new
+
+          //
+          if (message === '') {
+            return
+          }
+
+          // initialize comment array
+          if (!scope.comments[scope.selected_comments_id]) {
+            scope.comments[scope.selected_comments_id] = {}
+          }
+
+          if (scope.editingComment) {
+            var k = scope.editingComment
+            scope.comments[scope.selected_comments_id][k].message = message
+            scope.comments[scope.selected_comments_id][k].updated_at = new Date().getTime()
+          } else {
+            // push comment object
+            var k = 'c-' + new Date().getTime() + '-' + (Math.random() + '').replace('0.', '')
+            scope.comments[scope.selected_comments_id][k] = {
+              user_id: scope.author.id,
+              user_name: scope.author.first_name,
+              created_at: new Date().getTime(),
+              message: message
+            }
+          }
+
+          // console.log('scope.comments', scope.comments)
+
+          //
+          scope.comment_message_new = ''
+          this.editingComment = null
+
+          if (binding.value.onAddComment) {
+            binding.value.onAddComment()
+          }
+        }
+      },
+      mounted: function () {
+        if (binding.value && binding.value.onMounted) {
+          binding.value.onMounted(this)
+        }
+
+        el.addEventListener('mouseup', function (e) {
+          binding.vm.selected_comments_id = null
+          if (e.target && e.target.matches('.commentbase-comment-highlight')) {
+            binding.vm.selected_comments_id = e.target.dataset.commentsId
+            return
+          }
+          var sel, range
+          var id = ('comments-' + Math.random()).replace('.', '')
+
+          if (window.getSelection) {
+            sel = window.getSelection()
+            if (sel.getRangeAt && sel.rangeCount) {
+              range = window.getSelection().getRangeAt(0)
+
+              if (range.toString().trim() === '') {
+                return
+              }
+
+              binding.vm.selected_comments_id = id
+
+              var html = '<span class="commentbase-comment-highlight" style="font-weight: inherit; background: orange; color: #fff;" data-comments-id="' + id + '">' + range + '</span>'
+              range.deleteContents()
+
+              var el = document.createElement('div')
+              el.innerHTML = html
+              var frag = document.createDocumentFragment()
+              var node
+              while ((node = el.firstChild)) {
+                frag.appendChild(node)
+              }
+              range.insertNode(frag)
+            }
+          }
+        })
+      }
+    })
+  }
+})
+
 export default {
-  name: 'books-i-read-chapter-details',
+  name: 'chapter-details',
   props: ['properties'],
   data: function () {
+    var scope = this
     return {
       chapter_version: {
         chapter_id: null,
@@ -58,18 +260,35 @@ export default {
         active: 'content'
       },
       busy: false,
-      tempVersionDesc: ''
+      tempVersionDesc: '',
+      commentbased_params: {
+        onMounted: (vm) => {
+          scope.commentbased_vm = vm
+          vm.setAuthor(this.getAuthor)
+          vm.setCommentsJSON(this.comments)
+        },
+        onAddComment: function () {
+          scope.saveComments()
+        }
+      }
     }
   },
   components: {
     TinyMCE,
-    BooksIReadChapterScenes
+    ChapterScenes,
+    ChapterVersions,
+    ChapterCompareVersions
   },
   computed: {
     getChapterContent: function () {
       var scope = this
       var chapterID = scope.page.data.chapter.uuid
       return this.$store.getters.getChapterContent(chapterID)
+    },
+    comments: function () {
+      var scope = this
+      var chapterID = scope.page.data.chapter.uuid
+      return this.$store.getters.getChapterComments(chapterID)
     },
     book: function () {
       return this.properties.book
@@ -81,6 +300,10 @@ export default {
       var stillUtc = moment.utc('2020-04-09 13:51:40').toDate()
       var date = moment(stillUtc).local().format('YYYY-MM-DD HH:mm:ss')
       return moment('2020-04-09 21:51:40').utc().format('YYYY-MM-DD HH:mm:ss').toString() + ' ---> ' + date
+    },
+    getAuthor: function () {
+      var scope = this
+      return scope.$store.getters.getAuthor
     }
   },
   methods: {
@@ -93,78 +316,47 @@ export default {
       var scope = this
       scope.tab.active = active
     },
-    newVersion: function () {
+    saveComments () {
       var scope = this
-      this.busy = true
+      var chapterID = scope.page.data.chapter.uuid
 
-      scope.chapter_version.change_description = ''
-      if (scope.chapter_version.id) {
-        delete (scope.chapter_version.id)
-        delete (scope.chapter_version.uuid)
-      }
-    },
-    saveNewVersion () {
-      var scope = this
-
+      scope.chapter_version.chapter_id = chapterID
+      scope.chapter_version.uuid = this.$store.getters.getChapterVersionUUID(chapterID)
       scope.chapter_version.change_description = scope.tempVersionDesc
-      scope.chapter_version.content = scope.getChapterContent
-      scope.chapter_version.chapter_id = scope.page.data.chapter.uuid
+      scope.chapter_version.content = this.commentbased_vm.getContent()
+      scope.chapter_version.comments = this.commentbased_vm.getCommentsJSON()
+
+      /*
+      var data = Object.assign({}, scope.chapter_version, {
+        chapter_id: chapterID,
+        uuid: this.$store.getters.getChapterVersionUUID(chapterID),
+        change_description: scope.tempVersionDesc,
+        content: this.commentbased_vm.getContent(),
+        comments: this.commentbased_vm.getCommentsJSON()
+      })
+      */
 
       scope.axios
-        .post('http://localhost:3000/chapter-versions', scope.chapter_version)
+        .post('http://localhost:3000/chapter-versions/comment', scope.chapter_version)
         .then(response => {
           if (response.data) {
             // TODO: Insert vuex code that will refresh the chapter version
             scope.tab.active = 'content'
             scope.$store.dispatch('loadVersionsByChapter', scope.page.data.chapter.uuid)
             this.busy = false
+            /*
             window.swal.fire({
               position: 'center',
               icon: 'success',
-              title: this.$tc('CHAPTER', 1) + ' ' + this.$tc('VERSION', 1) + ' ' + this.$t('SUCCESSFULY_SAVED'),
+              title: this.$t('CHAPTER') + ' ' + this.$t('VERSION') + ' ' + this.$t('SUCCESSFULY_SAVED'),
               showConfirmButton: false,
               timer: 1500
             }).then(() => {
-              scope.tab.active = 'versions'
+              scope.tab.active = 'content'
             })
+            */
           }
         })
-    },
-    deleteChapter: function (chapter) {
-      var scope = this
-      window.swal.fire({
-        title: this.$t('ARE_YOU_SURE'),
-        text: this.$t('YOU_WONT_BE_ABLE_TO_REVERT_THIS'),
-        icon: 'warning',
-        showCancelButton: true,
-        confirmButtonColor: '#3085d6',
-        cancelButtonColor: '#d33',
-        confirmButtonText: this.$t('YES_DELETE_IT'),
-        cancelButtonText: this.$t('CANCEL')
-      }).then((result) => {
-        if (result.value) {
-          scope.axios
-            .delete('http://localhost:3000/chapters/' + chapter.uuid)
-            .then(response => {
-              if (response.data) {
-                window.swal.fire({
-                  position: 'center',
-                  icon: 'success',
-                  title: this.$t('RECORD_SUCCESSFULY_DELETED'),
-                  showConfirmButton: false,
-                  timer: 1500
-                }).then(() => {
-                  scope.$store.dispatch('removeChapterFromList', chapter)
-                  scope.CHANGE_COMPONENT({tabKey: 'chapter-listing-' + chapter.book_id, tabComponent: 'chapter-listing', tabData: { uuid: chapter.book_id }, tabTitle: this.$tc('CHAPTER', 2) + ' - ' + this.$t('LIST'), tabIndex: scope.$store.getters.getActiveTab})
-                })
-              }
-            })
-        }
-      })
-    },
-    exportContent: function () {
-      var scope = this
-      ipcRenderer.send('EXPORT-CONTENT-DOCX', {content: scope.getChapterContent, defaultfilename: scope.page.title + ' - ' + this.$t('CONTENT')})
     }
   },
   beforeUpdate () {
@@ -172,15 +364,19 @@ export default {
   },
   mounted () {
     var scope = this
+
     scope.page.data = scope.properties
     scope.page.title = scope.properties.chapter.title
-    console.log('PROPERTIES')
-    console.log(scope.properties)
+    // console.log('PROPERTIES')
+    // console.log(scope.properties)
     scope.$store.dispatch('loadScenesByChapter', scope.properties.chapter.uuid)
     scope.$store.dispatch('loadVersionsByChapter', scope.properties.chapter.uuid)
 
     setTimeout(function () {
-      scope.page.is_ready = true
+      Vue.nextTick(function () {
+        scope.page.is_ready = true
+        scope.changeTab('content')
+      })
     }, 300)
   }
 }
