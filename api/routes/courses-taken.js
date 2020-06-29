795cc52d9e55e28d7eb55eb0764dc39a4bc68324
John Paul Mayol <rancor.developer@gmail.com>
2020-06-29 18:01:45 +0800

    [CHG] - refactor course taken, package courses, lesson document

--- api/routes/courses-taken.js
+++ api/routes/courses-taken.js
@@ -1,21 +1,52 @@
 'use strict'
 const path = require('path')
-
 const express = require('express')
 const router = express.Router()
 
 const { CourseTakenController } = require(path.join(__dirname, '..', 'controllers'))
 
-router.get('/:userId/assignments', async function (req, res) {
-  let data = {
-    userId: req.params.userId
-  }
-
-  const assignments = await CourseTakenController.getAssignments(data)
+router.post('/', async function (req, res) {
+  const courseTaken = await CourseTakenController.save(req.body)
 
   res
     .status(200)
-    .json(assignments)
+    .json(courseTaken)
+})
+
+router.delete('/:courseId', async function (req, res) {
+  const courseTaken = await CourseTakenController.delete(req.params.courseId)
+
+  res
+    .status(200)
+    .json(courseTaken)
+})
+
+router.get('/syncable', async function (req, res) {
+  const rows = await CourseTakenController.getSyncable(req.query.userID)
+
+  res
+    .status(200)
+    .json(rows)
+})
+
+router.post('/sync', async function (req, res) {
+  const row = await CourseTakenController.sync(req.body)
+
+  res
+    .status(200)
+    .json(row)
+})
+
+router.get('/:courseId', async function (req, res) {
+  const courseTaken = await CourseTakenController.getByCourseId(req.params.courseId)
+
+  if (courseTaken.pictures) {
+    courseTaken.picture_src = 'file://' + path.resolve(resourcePath, 'resources', 'images', 'courses', course.pictures)
+  }
+
+  res
+    .status(200)
+    .json(courseTaken)
 })
 
 module.exports = router
