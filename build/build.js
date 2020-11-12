0b6a68e4d934997ed518e55d9254af13950385eb
John Paul Mayol <rancor.developer@gmail.com>
2020-11-12 21:49:36 +0800

    update repo name

--- build/build.js
+++ build/build.js
@@ -62,7 +62,10 @@ let buildOptions = {
       "private": true,
       "token": "dfd1c61fcb090ecba24909875e177c5326ad449d",
       "owner": "rancorfloydz",
-      "repo": "easywrite-mac"
+      "repo": "easywrite-se-dev",
+      // "repo": "easywrite-se-live"
+      // "repo": "easywrite-no-dev"
+      // "repo": "easywrite-no-live"
     }],
     "provisioningProfile": path.resolve('build/certificate/EasyWrite.provisionprofile'),
     "hardenedRuntime" : true,
@@ -86,8 +89,10 @@ let buildOptions = {
       "private": true,
       "token": "dfd1c61fcb090ecba24909875e177c5326ad449d",
       "owner": "rancorfloydz",
-      //"repo": "easywrite-v2-updater"
-      "repo": "easywrite"
+      "repo": "easywrite-se-dev",
+      // "repo": "easywrite-se-live"
+      // "repo": "easywrite-no-dev"
+      // "repo": "easywrite-no-live"
     }],
     "certificateFile" : "build/certificate/easywrite.pfx",
     "verifyUpdateCodeSignature" : true,
