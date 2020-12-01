cee77e88ad826255b6b9836705b1522efe9fa645
John Paul Mayol <rancor.developer@gmail.com>
2020-12-01 12:59:41 +0800

    fix autosync disable

--- src/stores/tab.js
+++ src/stores/tab.js
@@ -5,7 +5,7 @@ export default {
     tabs: {
       active_index: 0,
       items: [
-        { key: 'dashboard', title: 'DASHBOARD', component: 'dashboard', data: { autosync: !true }, refresh: false, modified: false }
+        { key: 'dashboard', title: 'DASHBOARD', component: 'dashboard', data: { autosync: true }, refresh: false, modified: false }
       ]
     },
     mainSideNavTab: {
