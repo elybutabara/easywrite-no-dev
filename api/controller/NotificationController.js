0d680a85042a2952200a4feb9172bea2536c1a79
Shellamae Bailio <shellabailio.97@gmail.com>
2020-12-02 15:33:08 +0800

    get notifications in controller

--- api/controller/NotificationController.js
+++ api/controller/NotificationController.js
@@ -1,12 +1,14 @@
 'use strict'
+const { query } = require('express')
 const path = require('path')
 
-const { User, Notification } = require(path.join(__dirname, '..', 'models'))
+const { User, Notification, Book, Author, Chapter, Scene } = require(path.join(__dirname, '..', 'models'))
 
 class NotificationController {
-  static async getAll (authorUuid) {
-    const notification = this.notifications(authorUuid)
-    const invitations = this.invitations(authorUuid)
+  static async getAll (authorUuid) { 
+
+    const notification = await this.notifications(authorUuid)
+    const invitations = await this.invitations(authorUuid)
     return {
       'notifications': notification,
       'invitations': invitations
@@ -14,14 +16,105 @@ class NotificationController {
   }
 
   static async notifications (authorUuid) {
-    // const notificationType = ['feedback', 'personalize-notification', 'reader-directory', 'book', 'book-directory', 'book_invite_decision', 'notif', 'chapter_comment', 'scene_comment']
-    var notification = Notification.query()
-      .where('to', authorUuid)
-      // .whereIn('type', notificationType)
-      // .whereNotIn('action', ['invite'])
-      .orderBy('created_at', 'desc')
 
+    /**
+     * QUERY HERE | Get notifications | feedback and comments only
+     */
+
+    const notificationType = ['feedback', 'chapter_comment', 'scene_comment']
+    var notification = await Notification.query()
+      .where(`to`, authorUuid)
+      .whereIn('type', notificationType)
+      .orderBy(`created_at`, 'desc')
+
+    var data = [];
+
+    for (let i = 0; i < notification.length; i++){
+ 
+      // get sender
+      const from = await Author.query() 
+                  .findById(notification[i].from)
+      console.log('alias', from)
+      const alias = from ? from.alias : 'Anonymous'
+      
+      notification[i].alias = alias;
+
+      const book = await Book.query()
+                  .withGraphJoined('author')
+                  .where('books.uuid', notification[i].book_id)
+
+      notification[i].book = book ? book[0] : null;
+
+      if(notification[i].type === 'feedback' && (notification[i].action == 'post' || notification[i].action === 'response')){
+
+        if(notification[i].parent_name == 'chapter'){
+
+          const chapter = await Chapter.query()
+                        .withGraphJoined('chapter_version')
+                        .modifyGraph('chapter_version', builder => {
+                          builder.whereNull('deleted_at')
+                          builder.orderBy('created_at')
+                        })
+                        .where('book_chapters.uuid', notification[i].parent_id)
+
+          notification[i].chapter = chapter ? chapter[0] : null;
+
+        }else if(notification[i].parent_name == 'scene'){
+          
+          const scene = await Scene.query()
+                      .withGraphJoined('book')
+                      .withGraphJoined('chapter')
+                      .whereNull('book.deleted_at')
+                      .whereNull('book_scenes.deleted_at')
+                      .where('book_scenes.uuid', notification[i].parent_id)
+                      .whereNull('chapter.deleted_at')
+
+          notification[i].scene = scene ? scene[0] : null;
+          notification[i].chapter = scene ? scene[0].chapter : null;
+
+        }
+
+      }else if(notification[i].type === 'notif' && notification[i].action === 'response'){
+
+        notification[i].message = from[0].alias + 'Sent you a message'
+        return notification;
+
+      }else if(notification[i].type === 'chapter_comment' && notification[i].action == 'post'){
+
+        const chapter = await Chapter.query()
+        .withGraphJoined('chapter_version')
+        .modifyGraph('chapter_version', builder => {
+          builder.whereNull('deleted_at')
+          builder.orderBy('created_at')
+        })
+        .where('book_chapters.uuid', notification[i].parent_id)
+
+        notification[i].chapter = chapter ? chapter[0] : null;
+        notification[i].author = from ? from[0] : null;
+
+        if(!chapter) return null;
+
+      } else if(notification[i].type === 'scene_comment' && notification[i].action == 'post'){
+
+        const scene = await Scene.query()
+        .whereNull('book_scenes.deleted_at')
+        .where('book_scenes.uuid', notification[i].parent_id)
+
+        notification[i].scene = scene ? scene[0] : null;
+        notification[i].chapter = scene ? scene[0].chapter : null;
+        notification[i].author = from ? from[0] : null;
+
+        if(!scene) return null;
+
+      } else {
+        
+        notification[i].author = from ? from[0] : null;
+
+      }
+    }
+    
     return notification
+
   }
 
   static async invitations (authorUuid) {
