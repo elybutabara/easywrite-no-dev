dc33e1094191ada5cb5a2479356f0f22c245605f
Shellamae Bailio <shellabailio.97@gmail.com>
2020-12-08 09:24:51 +0800

    Fix Importing docx (book, scene,chapter)  - some format not accepted

--- starter-extensions/imports/docx/import-docx-per-content.js
+++ starter-extensions/imports/docx/import-docx-per-content.js
@@ -19,29 +19,51 @@ exports.initMainWindow = (window) => {
 
       } else {
         var outputFile = result.filePaths
+        var html = null
 
         fs.readFile(outputFile[0], (err, data) => {
+
           if (err) throw err
 
-          var options = {
-            styleMap: [
-              'u => u'
-            ]
+          // check if there is altChunk
+          if(data.indexOf("<w:altChunk r:id=\"htmlChunk\" />") >= 0){ // Do not use mammoth
+            
+            html = data.toString().substring(data.toString().indexOf("<body>"), data.toString().indexOf("</body>"))
+
+            if (type === 'book') {
+              MainWindow.webContents.send('GET-DOCX-CONTENT-BOOK', html)
+            } else if (type === 'chapter') {
+              MainWindow.webContents.send('GET-DOCX-CONTENT-CHAPTER', html)
+            } else if (type === 'scene') {
+              MainWindow.webContents.send('GET-DOCX-CONTENT-SCENE', html)
+            }
+
+          }else{ //use mammoth
+            
+            var options = {
+              styleMap: [
+                'u => u'
+              ]
+            }
+  
+            mammoth.convertToHtml(data, options)
+              .then(function (result) {
+                html = result.value // The generated HTML 
+                // var messages = result.messages // Any messages, such as warnings during conversion
+
+                if (type === 'book') {
+                  MainWindow.webContents.send('GET-DOCX-CONTENT-BOOK', html)
+                } else if (type === 'chapter') {
+                  MainWindow.webContents.send('GET-DOCX-CONTENT-CHAPTER', html)
+                } else if (type === 'scene') {
+                  MainWindow.webContents.send('GET-DOCX-CONTENT-SCENE', html)
+                }
+
+              })
+              .done()
+
           }
-          mammoth.convertToHtml(data, options)
-            .then(function (result) {
-              var html = result.value // The generated HTML
-              var messages = result.messages // Any messages, such as warnings during conversion
-              // console.log(messages)
-              if (type === 'book') {
-                MainWindow.webContents.send('GET-DOCX-CONTENT-BOOK', html)
-              } else if (type === 'chapter') {
-                MainWindow.webContents.send('GET-DOCX-CONTENT-CHAPTER', html)
-              } else if (type === 'scene') {
-                MainWindow.webContents.send('GET-DOCX-CONTENT-SCENE', html)
-              }
-            })
-            .done()
+
         })
       }
     }).catch(err => {
