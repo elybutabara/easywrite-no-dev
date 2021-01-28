fd54fb1456342993023f393786456b2d3e9599c5
John Paul Mayol <rancor.developer@gmail.com>
2021-01-29 00:21:47 +0800

    add notifcation to chapter details

--- src/pages/views/chapters/chapter-details.vue
+++ src/pages/views/chapters/chapter-details.vue
@@ -153,7 +153,8 @@ export default {
       },
       exportOnProgress: false,
       exportLoading: this.$t('Loading'),
-      show_feedbacks: false
+      show_feedbacks: false,
+      notification: null
     }
   },
   components: {
@@ -334,6 +335,11 @@ export default {
       scope.show_feedbacks = true
     }
 
+    if (scope.properties.notification) {
+      scope.notification = scope.properties.notification
+    }
+
+    console.log(scope.notification)
     scope.page.data = scope.properties
     scope.page.title = scope.properties.chapter.title
     // console.log('PROPERTIES')
