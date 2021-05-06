8681ddeee50a73357b0c9072c9106ebdc52063cf
Kenji <codes.kenjimagto@gmail.com>
2021-05-06 16:01:46 +0800

    Fixes, Point to local

--- src/main.js
+++ src/main.js
@@ -140,9 +140,9 @@ let APP = {
   }
 }
 
-window.APP = APP.NO_DEV // DEVELOPEMENT APP
+window.APP = APP.LOCAL // DEVELOPEMENT APP
 if (process.env.NODE_ENV === 'production') {
-  window.APP = APP.NO_DEV // PRODUCTION APP
+  window.APP = APP.LOCAL // PRODUCTION APP
 }
 // window.$.fn.select2.defaults.set('theme', 'bootstrap')
 
